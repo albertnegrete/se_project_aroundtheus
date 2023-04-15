6bc2afcbab5bedc5e1f08ae2f4980ab3c9ece978
albertnegrete <albertnegrete@gmail.com>
2023-04-15 14:42:59 -0400

    connect modal buttons

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
 		link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
 	},
 ];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const closeProfileEditModal = document.querySelector("#modal-close");
+
+profileEditButton.addEventListener("click", () => {
+	profileEditModal.classList.add("modal_opened");
+});
+
+closeProfileEditModal.addEventListener("click", () => {
+	profileEditModal.classList.remove("modal_opened");
+});
