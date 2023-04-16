abac2a52e338956ee3bd86758d652ae7ce325cd0
albertnegrete <albertnegrete@gmail.com>
2023-04-16 17:30:22 -0400

    script: add functionality to modal

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,34 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const closeProfileEditModal = document.querySelector("#modal-close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+	"#profile-description-input"
+);
+
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopUp() {
+	profileEditModal.classList.remove("modal_opened");
+}
 
 profileEditButton.addEventListener("click", () => {
+	profileTitleInput.value = profileTitle.textContent;
+	profileDescriptionInput.value = profileDescription.textContent;
+
 	profileEditModal.classList.add("modal_opened");
 });
 
 closeProfileEditModal.addEventListener("click", () => {
-	profileEditModal.classList.remove("modal_opened");
+	closePopUp();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log("Form Submitted!");
+	profileTitle.textContent = profileTitleInput.value;
+	profileDescription.textContent = profileDescriptionInput.value;
+	closePopUp();
 });
